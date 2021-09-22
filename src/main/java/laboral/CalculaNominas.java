babf37e31794a2f4cbadfcd4ce884738fa6db0cd
luiruirom <xiwensix@gmail.com>
2021-09-22 13:40:17 +0200

    Inicio parte 2

--- src/main/java/laboral/CalculaNominas.java
+++ src/main/java/laboral/CalculaNominas.java
@@ -1,18 +1,21 @@
 package laboral;
 
+import java.io.*;
+import java.util.*;
 /**
  * Clase main con un método para imprimir los datos de los empleados y con un método main que prueba los métodos de todas las clases
  * @author luiruirom
  *
  */
 public class CalculaNominas {
-	
 	/**
 	 * Método main en el que se crean dos empleados y se prueban los métodos de las demás clases
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		try {
+		
+		/* Parte 1
+		 * try {
 			Empleado james = new Empleado("James Cosling", "32000032G", 'M', 4, 7);
 			Empleado ada = new Empleado("Ada Lovelace", "32000031R", 'F');
 			escribe(james, ada);
@@ -21,7 +24,46 @@ public class CalculaNominas {
 			escribe(james, ada);
 		} catch (DatosNoCorrectosException ex) {
 			System.out.println(ex.getMessage());
-		}
+		} 
+		 */
+		File archivo = null;
+	    FileReader fr = null;
+	    BufferedReader br = null;
+		ArrayList<String> listaEmpleados = new ArrayList<>();
+		
+	    try {
+	        archivo = new File ("/home/luiruirom/eclipse-workspace/pruebadwes/src/main/java/laboral/empleados.txt");
+	        fr = new FileReader (archivo);
+	        br = new BufferedReader(fr);
+	        
+	        String linea;
+	        while((linea=br.readLine())!=null)
+	           System.out.println(linea);
+	     }
+	     catch(Exception e){
+	        e.printStackTrace();
+	     }finally{
+	        try{                    
+	           if(null != fr){   
+	              fr.close();     
+	           }                  
+	        }catch (Exception e2){ 
+	           e2.printStackTrace();
+	        }
+	     }
+	    
+	    try {
+	        BufferedWriter out = null;
+	        out = new BufferedWriter(new FileWriter("sueldos.dat"));                                 
+	        out.flush();
+	        out.close();
+	    } catch (FileNotFoundException e) {
+	        System.out.println("ERROR al crear el fichero");
+	        return;
+	    } catch (IOException e) {
+	        System.out.println("ERROR al escribir en el fichero");
+	        return;
+	    }
 	}
 	
 	/**
